fefc112e5ef89bb2d4d07a588b97c36bb654274f
caroline-pinte <caroline.pinte@irisa.fr>
2021-03-24 15:56:19 +0100

    adapting params : lambda, frontal elec

--- Matlab/pred_NF_from_eeg_fmri_1model_AVC.m
+++ Matlab/pred_NF_from_eeg_fmri_1model_AVC.m
@@ -166,8 +166,8 @@ end
 % Removing some electrods (the noisy ones, like occipital) from the design matrix of both steps:
 disp(['  *  Removing some electrods (the noisy ones, like occipital) from the design matrix of both steps:']);
 
-motor_channels = [5,6,18,21,22,23,24,25,26,27,28,35,36,41,42,43,44,49,50,64]; % electrods to keep, base 64 already
-frontal_channels = [33 34 17]; % electrods to keep, base 64 already. Removed for patients.
+motor_channels = [5,6,17,18,21,22,23,24,25,26,27,28,33,34,35,36,41,42,43,44,49,50,64]; % electrods to keep, base 64 already
+%frontal_channels = [33 34 17]; % electrods to keep, base 64 already. Removed for patients.
 all_channels = [1:64];
 ind_elect_eeg_exclud = 1:64; % electrodes to exclude
 ind_elect_eeg_exclud([motor_channels ])=[];
@@ -330,7 +330,7 @@ disp(['  **  Estimating regularisation parameter lambda for method ' reg_functio
 if strcmp(reg_function, 'lasso')
     lambdas=[0.1:0.2:10];
 elseif strcmp(reg_function, 'fistaL1')
-    lambdas=[0:100:5000]; %initial values
+    lambdas=[0:80:2000]; %initial values
     %lambdas=[0:500:50000]; % test
 end
 
--- Matlab/script_predNF_AVC.m
+++ Matlab/script_predNF_AVC.m
@@ -1,13 +1,13 @@
 
-patients={'P002';'P003'; 'P004'; 'P015'; 'P017'};
+patients={'XP216'};
 sessions={'S1s1'; 'S1s2'; 'S2s1'; 'S3s1'};
 learn_runs={'NF1'; 'NF2'; 'NF3'};
-test_runs=learn_runs;
+test_runs={'NF3'};
 for p=1:length(patients)
     for s=1:length(sessions)
         for l=1:length(learn_runs)
             for t=1:length(test_runs)
-                res_path=['C:/Users/cpinte/Documents/Results/Res_', patients{p} ,'_s' ,sessions{s}, '_l', learn_runs{l}, '_t', test_runs{t}, '/Res_', patients{p} ,'_s' ,sessions{s}, '_l', learn_runs{l}, '_t', test_runs{t}, '.mat'];
+                res_path=['C:/Users/cpinte/Documents/Results/Sujets_sains/Res_', patients{p} ,'_s' ,sessions{s}, '_l', learn_runs{l}, '_t', test_runs{t}, '/Res_', patients{p} ,'_s' ,sessions{s}, '_l', learn_runs{l}, '_t', test_runs{t}, '.mat'];
                 Res=pred_NF_from_eeg_fmri_1model_AVC(patients{p}, sessions{s}, learn_runs{l}, test_runs{t},'fmri');              
                 save(res_path,'Res');
             end
